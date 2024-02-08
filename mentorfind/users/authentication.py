f14cfc137ad5c490c3019f0d85379747fb09b4b5
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-02-08 17:40:52 +0200

    -Wrote a registration class -Wrote CustomUserSerializerRegistration -Changed the name CustomUserSerializerLogin

--- mentorfind/users/authentication.py
+++ mentorfind/users/authentication.py
@@ -2,38 +2,39 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializerLogin
+from .serializers import CustomUserSerializerRegistration
 from .models import CustomUser
+
 
 class RegisterView(generics.CreateAPIView):
     """
         Registration function created with standart DRF registration    
     """
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializerRegistration
+    permission_classes = (permissions.AllowAny,)
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                password = request.data.get('password')
+                user.set_password(password) #ДУЖЕ ВАЖЛИВА СТРОЧКА. Вона зашифровує пароль, а якщо його не шифрувати то нічого не робить
+                user.save()
 
-    #queryset = CustomUser.objects.all()
-    #serializer_class = CustomUserSerializer
-    #permission_classes = (permissions.AllowAny,)
+                token = Token.objects.create(user=user)
+                return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    #def create(self, request, *args, **kwargs):
-    #    serializer = self.get_serializer(data=request.data)
-    #    if serializer.is_valid():
-    #        user = serializer.save()
-    #        if user:
-    #            password = request.data.get('password')
-    #            user.set_password(password) #ДУЖЕ ВАЖЛИВА СТРОЧКА. Вона зашифровує пароль, а якщо його не шифрувати то нічого не робить
-    #            user.save()
-    #
-    #            token = Token.objects.create(user=user)
-    #            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
-    #    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(generics.GenericAPIView):
     """
         Login function created with standart DRF registration    
     """
 
-    serializer_class = CustomUserSerializer
+    serializer_class = CustomUserSerializerLogin
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
--- mentorfind/users/serializers.py
+++ mentorfind/users/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import CustomUser
-from django.core.validators import MinLengthValidator 
+from django.core.validators import MinLengthValidator, EmailValidator
 from rest_framework.validators import UniqueValidator
 from .validators import uppercase_letter_validation
 
-class CustomUserSerializer(serializers.ModelSerializer):
+
+class CustomUserSerializerLogin(serializers.ModelSerializer):
     """
         Serializer for the CustomUser model
     """
@@ -16,6 +17,35 @@ class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         validators=[
             MinLengthValidator(5), 
+            uppercase_letter_validation
+        ]
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class CustomUserSerializerRegistration(serializers.ModelSerializer):
+    username = serializers.CharField(
+        validators=[
+            MinLengthValidator(5)
+        ]
+    )
+
+    password = serializers.CharField(
+        validators=[
+            UniqueValidator(queryset=CustomUser.objects.all()),
+            MinLengthValidator(5),
+            uppercase_letter_validation
+        ]
+    )
+
+    email = serializers.CharField(
+        validators=[
+            UniqueValidator(queryset=CustomUser.objects.all()),
+            EmailValidator(),
             uppercase_letter_validation
         ]
     )
