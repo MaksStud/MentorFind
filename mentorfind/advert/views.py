7a69976eb4be1236360bd49a5bdf97271ee356a8
IvanFranCOCK <49644012+Vitya77@users.noreply.github.com>
2024-02-28 18:51:45 +0200

    Add token authentication and updeted db to merge correctly

--- mentorfind/advert/views.py
+++ mentorfind/advert/views.py
@@ -1,11 +1,16 @@
 # views.py
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
 from rest_framework.response import Response
 
 class AdvertisementViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
 
