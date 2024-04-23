f45e0e8230e5007d2d023fadd64afc7e8378a783
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-04-23 20:37:17 +0300

    reduced url size for accessing the view history

--- mentorfind/viewhistory/urls.py
+++ mentorfind/viewhistory/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ViewHistoryViewSet
 
 urlpatterns = [
-    path('viewhistory/', ViewHistoryViewSet.as_view({'post': 'create', 'get': 'list'}, name='viewhistory')),
+    path('', ViewHistoryViewSet.as_view({'post': 'create', 'get': 'list'}, name='viewhistory')),
 ]
