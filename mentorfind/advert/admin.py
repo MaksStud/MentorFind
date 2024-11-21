c6a1c5f9971c99c89197e31540a77a3bcc8cea42
MaksStud <119805355+MaksStud@users.noreply.github.com>
2024-11-21 13:11:22 +0200

    Added: admin panel

--- mentorfind/advert/admin.py
+++ mentorfind/advert/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Advertisement, Review
 
-# Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
--- mentorfind/users/admin.py
+++ mentorfind/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'email')
+    search_fields = ('email', )
+
